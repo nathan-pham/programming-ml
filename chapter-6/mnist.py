4add8cfea5bc8e4984a5e945cef0601147ebf387
Nathan Pham <nathanpham.me@gmail.com>
2021-07-06 09:46:47 -0700

    load labels

--- chapter-6/mnist.py
+++ chapter-6/mnist.py
@@ -5,16 +5,17 @@ import os
 
 __dirname = os.path.dirname(__file__)
 
-TRAIN_DATASET = os.path.join(__dirname, "./dataset/train-images-idx3-ubyte.gz")
-TEST_DATASET = os.path.join(__dirname, "./dataset/t10k-images-idx3-ubyte.gz")
-
 def load_images(filename):
+    filename = os.path.join(__dirname, "./dataset", filename)
+
     with gzip.open(filename, "rb") as f:
         _ignored, n_images, columns, rows = struct.unpack(">IIII", f.read(16))
         all_pixels = np.frombuffer(f.read(), dtype=np.uint8)
         return all_pixels.reshape(n_images, columns * rows)
 
 def load_labels(filename):
+    filename = os.path.join(__dirname, "./dataset", filename)
+
     with gzip.open(filename, "rb") as f:
         f.read(8) # skip header bytes
         all_labels = f.read()
@@ -26,8 +27,8 @@ def prepend_bias(X):
 def encode_fives(Y):
     return (Y == 5).astype(int)
 
-X_TRAIN = prepend_bias(load_images(TRAIN_DATASET))
-Y_TRAIN = encode_fives(load_labels(TRAIN_DATASET))
+X_TRAIN = prepend_bias(load_images("train-images-idx3-ubyte.gz"))
+Y_TRAIN = encode_fives(load_labels("train-labels-idx1-ubyte.gz"))
 
-X_TEST = prepend_bias(load_images(TEST_DATASET))
-Y_TEST = encode_fives(load_labels(TEST_DATASET))
+X_TEST = prepend_bias(load_images("t10k-images-idx3-ubyte.gz"))
+Y_TEST = encode_fives(load_labels("t10k-labels-idx1-ubyte.gz"))
